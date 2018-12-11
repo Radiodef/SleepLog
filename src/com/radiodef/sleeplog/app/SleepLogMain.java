ce219aeaf85bf5262ea45ae06384dded0734271d
David <acynicalpie@gmail.com>
2018-12-11 13:56:27 -0500

    add main

---
+++ src/com/radiodef/sleeplog/app/SleepLogMain.java
@@ -0,0 +1,10 @@
+package com.radiodef.sleeplog.app;
+
+public final class SleepLogMain {
+    private SleepLogMain() {
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(SleepLogMain.class);
+    }
+}
