92239355a36a5532eff695bed8ff75049816514f
David <acynicalpie@gmail.com>
2018-12-11 17:37:11 -0500

    automatically log class name and method

--- src/main/java/com/radiodef/sleeplog/app/Log.java
+++ src/main/java/com/radiodef/sleeplog/app/Log.java
@@ -1,10 +1,19 @@
 package com.radiodef.sleeplog.app;
 
+import com.radiodef.sleeplog.util.*;
+
 final class Log {
     private Log() {
     }
     
     static void note(String fmt, Object... args) {
+        Tools.getCaller().ifPresent(c -> {
+            System.out.print(Tools.getSimpleName(c.getDeclaringClass()));
+            System.out.print('.');
+            System.out.print(c.getMethodName());
+            System.out.print(": ");
+        });
+        
         System.out.printf(fmt, args);
         System.out.println();
     }
