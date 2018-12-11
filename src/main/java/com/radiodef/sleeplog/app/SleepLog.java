47e10d784a32c22e2a42416781ad5c1dac6494b4
David <acynicalpie@gmail.com>
2018-12-11 15:47:23 -0500

    add FX application + main

---
+++ src/main/java/com/radiodef/sleeplog/app/SleepLog.java
@@ -0,0 +1,15 @@
+package com.radiodef.sleeplog.app;
+
+import javafx.application.*;
+import javafx.stage.*;
+
+public final class SleepLog extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.show();
+    }
+}
