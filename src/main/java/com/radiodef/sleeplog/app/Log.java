d7631e3e65aad3930375422a124116290be5cd25
David <acynicalpie@gmail.com>
2018-12-11 20:44:39 -0500

    private note to no varargs

--- src/main/java/com/radiodef/sleeplog/app/Log.java
+++ src/main/java/com/radiodef/sleeplog/app/Log.java
@@ -3,13 +3,14 @@ package com.radiodef.sleeplog.app;
 import com.radiodef.sleeplog.util.*;
 
 import java.util.*;
+import org.apache.commons.lang3.ArrayUtils;
 
 final class Log {
     private Log() {
     }
     
     static void enter() {
-        note(Tools.getCaller(), "entered");
+        note(Tools.getCaller(), "entered", ArrayUtils.EMPTY_OBJECT_ARRAY);
     }
     
     static void note(String fmt, Object... args) {
@@ -17,7 +18,7 @@ final class Log {
     }
     
     @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
-    private static void note(Optional<StackWalker.StackFrame> caller, String fmt, Object... args) {
+    private static void note(Optional<StackWalker.StackFrame> caller, String fmt, Object[] args) {
         caller.ifPresent(c -> {
             System.out.print(Tools.getSimpleName(c.getDeclaringClass()));
             System.out.print('.');
