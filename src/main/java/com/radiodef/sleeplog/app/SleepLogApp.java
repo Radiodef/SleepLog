8d3b5821adbac4f7d33d865bb4975c0c2a408cc9
David <acynicalpie@gmail.com>
2018-12-12 13:54:50 -0500

    configure size and location of window

--- src/main/java/com/radiodef/sleeplog/app/SleepLogApp.java
+++ src/main/java/com/radiodef/sleeplog/app/SleepLogApp.java
@@ -13,6 +13,19 @@ public final class SleepLogApp extends Application {
     public void start(Stage primaryStage) {
         setUserAgentStylesheet(STYLESHEET_MODENA);
         
+        configure(primaryStage);
         primaryStage.show();
+    }
+    
+    private void configure(Stage stage) {
+        var bounds = Screen.getPrimary().getVisualBounds();
+        Log.note("primary screen bounds = %s", bounds);
+        
+        final double ratio = 0.5;
+        stage.setWidth(ratio * bounds.getWidth());
+        stage.setHeight(ratio * bounds.getHeight());
+        
+        stage.setX(bounds.getMinX() + (bounds.getWidth() - stage.getWidth()) / 2);
+        stage.setY(bounds.getMinY() + (bounds.getHeight() - stage.getHeight()) / 2);
     }
 }
