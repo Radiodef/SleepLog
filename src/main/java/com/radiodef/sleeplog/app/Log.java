85456d1c6118a8b70b7df1fea66bcfd6e95d2f7a
David <acynicalpie@gmail.com>
2018-12-19 18:18:23 -0500

    refactor, rename formatting note to notef

--- src/main/java/com/radiodef/sleeplog/app/Log.java
+++ src/main/java/com/radiodef/sleeplog/app/Log.java
@@ -3,26 +3,32 @@ package com.radiodef.sleeplog.app;
 import com.radiodef.sleeplog.util.*;
 
 import java.util.*;
-import org.apache.commons.lang3.ArrayUtils;
 
 public final class Log {
     private Log() {
     }
     
     public static void enter() {
-        note(Tools.getCaller(), "entered", ArrayUtils.EMPTY_OBJECT_ARRAY);
+        note(Tools.getCaller(), "entered", null, false);
     }
     
     public static void caught(Throwable x) {
         x.printStackTrace(System.out);
     }
     
-    public static void note(String fmt, Object... args) {
-        note(Tools.getCaller(), fmt, args);
+    public static void note(String msg) {
+        note(Tools.getCaller(), msg, null, false);
+    }
+    
+    public static void notef(String fmt, Object... args) {
+        note(Tools.getCaller(), fmt, args, true);
     }
     
     @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
-    private static void note(Optional<StackWalker.StackFrame> caller, String fmt, Object[] args) {
+    private static void note(Optional<StackWalker.StackFrame> caller,
+                             String str,
+                             Object[] args,
+                             boolean isFormat) {
         caller.ifPresent(c -> {
             System.out.print(Tools.getSimpleName(c.getDeclaringClass()));
             System.out.print('.');
@@ -30,7 +36,11 @@ public final class Log {
             System.out.print(": ");
         });
         
-        System.out.printf(fmt, args);
+        if (isFormat) {
+            System.out.printf(str, args);
+        } else {
+            System.out.print(str);
+        }
         System.out.println();
     }
 }
