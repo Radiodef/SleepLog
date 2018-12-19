0ed25cfe4ce06f1260e0dfe23ba55a9b27c0299a
David <acynicalpie@gmail.com>
2018-12-19 16:55:12 -0500

    create column type for Instant

--- src/main/java/com/radiodef/sleeplog/db/Database.java
+++ src/main/java/com/radiodef/sleeplog/db/Database.java
@@ -1,6 +1,7 @@
 package com.radiodef.sleeplog.db;
 
 import com.radiodef.sleeplog.app.*;
+import com.radiodef.sleeplog.util.*;
 
 import java.sql.*;
 
@@ -20,10 +21,46 @@ public class Database implements AutoCloseable {
         }
         
         this.conn = conn;
+        
+        if (conn != null) {
+            if (createInstantType()) {
+                Log.note("Instant type created, or already existed");
+            }
+        }
     }
     
     public boolean didConnect() {
         return conn != null;
+    }
+    
+    /**
+     * @return true if the type was created successfully
+     */
+    private boolean createInstantType() {
+        if (!didConnect())
+            return false;
+        
+        final String createTypeStatement =
+            "CREATE TYPE INSTANT EXTERNAL NAME '"
+                + java.time.Instant.class.getCanonicalName()
+                + "' LANGUAGE JAVA";
+        
+        try (var statement = conn.createStatement()) {
+            statement.execute(createTypeStatement);
+            
+        } catch (SQLException x) {
+            if (isTypeAlreadyExistsException(x)) {
+                Log.note(x.getMessage());
+            } else {
+                Log.caught(x);
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    private boolean isTypeAlreadyExistsException(SQLException x) {
+        return "X0Y68".equals(x.getSQLState());
     }
     
     @Override
