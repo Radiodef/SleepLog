57b9c206f9c6a34dece3493fd3c963ce317e09bd
David <acynicalpie@gmail.com>
2018-12-19 18:06:10 -0500

    refactor and add wipe() utility

--- src/main/java/com/radiodef/sleeplog/db/Database.java
+++ src/main/java/com/radiodef/sleeplog/db/Database.java
@@ -5,6 +5,7 @@ import com.radiodef.sleeplog.util.*;
 
 import java.sql.*;
 import java.time.*;
+import java.util.*;
 
 public final class Database implements AutoCloseable {
     private static final String DB_NAME = "sleeplog-db";
@@ -46,40 +47,21 @@ public final class Database implements AutoCloseable {
      * @return true if the type was created successfully
      */
     private boolean createInstantType() {
-        if (!didConnect())
-            return false;
-        
         final String createTypeStatement =
             "CREATE TYPE INSTANT EXTERNAL NAME '"
                 + Instant.class.getCanonicalName()
                 + "' LANGUAGE JAVA";
-        
-        try (var statement = conn.createStatement()) {
-            statement.execute(createTypeStatement);
-            Log.note("Instant data type created");
-            
-        } catch (SQLException x) {
-            if (isTypeAlreadyExistsException(x)) {
-                Log.note(x.getMessage());
-            } else {
-                Log.caught(x);
-                return false;
-            }
+        if (executeStatement(createTypeStatement, "X0Y68")) {
+            Log.note("Instant type created or already existed");
+            return true;
         }
-        return true;
-    }
-    
-    private boolean isTypeAlreadyExistsException(SQLException x) {
-        return "X0Y68".equals(x.getSQLState());
+        return false;
     }
     
     /**
      * @return true if the table exists
      */
     private boolean createDatesTable() {
-        if (!didConnect())
-            return false;
-        
         final String createTableStatement =
             "CREATE TABLE " + DATES_TABLE
             + "("
@@ -87,13 +69,23 @@ public final class Database implements AutoCloseable {
             + START_COL + " INSTANT,"
             + END_COL + " INSTANT"
             + ")";
+        // https://stackoverflow.com/a/5866339/2891664
+        if (executeStatement(createTableStatement, "X0Y32")) {
+            Log.note("Dates table created or already existed");
+            return true;
+        }
+        return false;
+    }
+    
+    private boolean executeStatement(String statement, String... okStates) {
+        if (!didConnect())
+            return false;
         
-        try (var statement = conn.createStatement()) {
-            statement.execute(createTableStatement);
-            Log.note("Dates table created");
-        
+        try (var s = conn.createStatement()) {
+            s.execute(statement);
+            
         } catch (SQLException x) {
-            if (isTableAlreadyExistsException(x)) {
+            if (Arrays.asList(okStates).contains(x.getSQLState())) {
                 Log.note(x.getMessage());
             } else {
                 Log.caught(x);
@@ -102,11 +94,6 @@ public final class Database implements AutoCloseable {
         }
         
         return true;
-    }
-    
-    private boolean isTableAlreadyExistsException(SQLException x) {
-        // https://stackoverflow.com/a/5866339/2891664
-        return "X0Y32".equals(x.getSQLState());
     }
     
     private PreparedStatement prepareInsertPeriodRow() {
@@ -175,6 +162,14 @@ public final class Database implements AutoCloseable {
         }
         
         return success;
+    }
+    
+    @SuppressWarnings("unused")
+    private void wipe() {
+        if (didConnect()) {
+            Log.note("table dropped = %b", executeStatement("DROP TABLE " + DATES_TABLE));
+            Log.note("instant dropped = %b", executeStatement("DROP TYPE INSTANT RESTRICT"));
+        }
     }
     
     @Override
