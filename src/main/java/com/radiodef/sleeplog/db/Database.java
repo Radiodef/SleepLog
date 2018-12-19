222f0c0dc06e34b80b67de4e4478f3ca253315da
David <acynicalpie@gmail.com>
2018-12-19 16:09:40 -0500

    create connection to Derby db

--- src/main/java/com/radiodef/sleeplog/db/Database.java
+++ src/main/java/com/radiodef/sleeplog/db/Database.java
@@ -1,4 +1,21 @@
 package com.radiodef.sleeplog.db;
 
+import com.radiodef.sleeplog.app.*;
+
+import java.sql.*;
+
 public class Database {
+    private static final String DB_NAME = "sleeplog-db";
+    
+    private volatile Connection conn;
+    
+    public Database() {
+        Log.enter();
+        
+        try {
+            conn = DriverManager.getConnection("jdbc:derby:" + DB_NAME + ";create=true");
+        } catch (SQLException x) {
+            Log.caught(x);
+        }
+    }
 }
