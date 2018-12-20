fdf267891390524973347c357111ce0f6f80bc83
David <acynicalpie@gmail.com>
2018-12-20 11:22:17 -0500

    add cell value factories and population

--- src/main/java/com/radiodef/sleeplog/app/DatabaseTablePane.java
+++ src/main/java/com/radiodef/sleeplog/app/DatabaseTablePane.java
@@ -1,6 +1,39 @@
 package com.radiodef.sleeplog.app;
 
+import com.radiodef.sleeplog.db.*;
+
+import javafx.scene.control.*;
+import javafx.scene.control.cell.*;
 import javafx.scene.layout.*;
 
+import java.time.*;
+import java.util.*;
+
 class DatabaseTablePane extends BorderPane {
+    private final Database db;
+    private final TableView<SleepPeriod> table;
+    
+    DatabaseTablePane(Database db) {
+        this.db = Objects.requireNonNull(db, "db");
+        
+        var idCol = new TableColumn<SleepPeriod, Integer>("ID");
+        var startCol = new TableColumn<SleepPeriod, Instant>("Start");
+        var endCol = new TableColumn<SleepPeriod, Instant>("End");
+        
+        idCol.setCellValueFactory(new PropertyValueFactory<>("id"));
+        startCol.setCellValueFactory(new PropertyValueFactory<>("start"));
+        endCol.setCellValueFactory(new PropertyValueFactory<>("end"));
+        
+        this.table = new TableView<>();
+        table.setEditable(false);
+        
+        Collections.addAll(table.getColumns(), idCol, startCol, endCol);
+        setCenter(table);
+        
+        update();
+    }
+    
+    void update() {
+        table.setItems(db.getAllSleepPeriods());
+    }
 }
