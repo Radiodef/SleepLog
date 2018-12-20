16d5582852ea801994f7a890cd903823a3165a9c
David <acynicalpie@gmail.com>
2018-12-20 11:18:07 -0500

    use fx properties

--- src/main/java/com/radiodef/sleeplog/db/SleepPeriod.java
+++ src/main/java/com/radiodef/sleeplog/db/SleepPeriod.java
@@ -3,28 +3,43 @@ package com.radiodef.sleeplog.db;
 import java.time.*;
 import java.util.*;
 
+import javafx.beans.property.*;
+
 import org.apache.commons.lang3.builder.*;
 
+@SuppressWarnings({"unused", "WeakerAccess"})
 public final class SleepPeriod {
-    private final int id;
-    private final Instant start;
-    private final Instant end;
+    private final ReadOnlyIntegerProperty id;
+    private final ReadOnlyObjectProperty<Instant> start;
+    private final ReadOnlyObjectProperty<Instant> end;
     
     public SleepPeriod(int id, Instant start, Instant end) {
-        this.id = id;
-        this.start = Objects.requireNonNull(start, "start");
-        this.end = Objects.requireNonNull(end, "end");
+        this.id = new SimpleIntegerProperty(this, "id", id);
+        this.start = new SimpleObjectProperty<>(this, "start", Objects.requireNonNull(start, "start"));
+        this.end = new SimpleObjectProperty<>(this, "end", Objects.requireNonNull(end, "end"));
     }
     
     public int getID() {
-        return id;
+        return id.intValue();
     }
     
     public Instant getStart() {
-        return start;
+        return start.get();
     }
     
     public Instant getEnd() {
+        return end.get();
+    }
+    
+    public ReadOnlyIntegerProperty idProperty() {
+        return id;
+    }
+    
+    public ReadOnlyObjectProperty<Instant> startProperty() {
+        return start;
+    }
+    
+    public ReadOnlyObjectProperty<Instant> endProperty() {
         return end;
     }
     
@@ -34,6 +49,7 @@ public final class SleepPeriod {
     }
     
     @Override
+    @SuppressWarnings("EqualsWhichDoesntCheckParameterClass")
     public boolean equals(Object o) {
         return EqualsBuilder.reflectionEquals(this, o);
     }
