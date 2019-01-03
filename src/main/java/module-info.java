ae26d9c22e594ef7e591c44c9d4bb56683b48dfb
David <acynicalpie@gmail.com>
2019-01-02 19:09:24 -0500

    export util

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module sleeplog {
     requires org.apache.commons.lang3;
     
     exports com.radiodef.sleeplog.app;
+    exports com.radiodef.sleeplog.util;
     
     opens com.radiodef.sleeplog.db to javafx.base, org.apache.commons.lang3;
 }
