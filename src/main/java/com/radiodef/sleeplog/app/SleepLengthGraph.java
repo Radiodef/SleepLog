f700df8a31e3c9d9c33e62206668287c6e70f9d5
David <acynicalpie@gmail.com>
2019-01-03 12:54:33 -0500

    sleep length graph is a pane

--- src/main/java/com/radiodef/sleeplog/app/SleepLengthGraph.java
+++ src/main/java/com/radiodef/sleeplog/app/SleepLengthGraph.java
@@ -5,17 +5,36 @@ import com.radiodef.sleeplog.util.*;
 
 import javafx.collections.*;
 import javafx.scene.chart.*;
+import javafx.scene.layout.*;
 import javafx.util.StringConverter;
 
 import java.util.*;
 import java.util.function.*;
 import java.time.*;
 
-final class SleepLengthGraph extends AreaChart<Number, Number> {
+final class SleepLengthGraph extends BorderPane {
     private static final int SECS_IN_HR = 60 * 60;
     private static final int SECS_IN_DAY = 60 * 60 * 24;
     
     private final Database db;
+    
+    private final AreaChart<Number, Number> chart;
+    
+    SleepLengthGraph(Database db) {
+        this.db = Objects.requireNonNull(db, "db");
+        
+        this.chart = new AreaChart<>(createXAxis(), createYAxis());
+        
+        update();
+        db.getAllSleepPeriods().addListener((ListChangeListener<SleepPeriod>) e -> update());
+        
+        setCenter(chart);
+        
+//        var series = createSeries();
+//        setData(Tools.observableArrayList(series));
+//
+//        setBounds();
+    }
     
     private static NumberAxis createXAxis() {
         var axis = new NumberAxis();
@@ -53,23 +72,9 @@ final class SleepLengthGraph extends AreaChart<Number, Number> {
         return axis;
     }
     
-    SleepLengthGraph(Database db) {
-        super(createXAxis(), createYAxis());
-        
-        this.db = Objects.requireNonNull(db, "db");
-        
-        update();
-        db.getAllSleepPeriods().addListener((ListChangeListener<SleepPeriod>) e -> update());
-        
-//        var series = createSeries();
-//        setData(Tools.observableArrayList(series));
-//
-//        setBounds();
-    }
-    
-    private Series<Number, Number> createSeries() {
+    private AreaChart.Series<Number, Number> createSeries() {
         Log.enter();
-        var series = new Series<Number, Number>();
+        var series = new AreaChart.Series<Number, Number>();
         series.setName("Sleep Duration");
         
         var periods = db.getAllSleepPeriods();
@@ -87,7 +92,7 @@ final class SleepLengthGraph extends AreaChart<Number, Number> {
         var minSeconds = Long.MAX_VALUE;
         var maxSeconds = Long.MIN_VALUE;
         
-        for (var data : getData().get(0).getData()) {
+        for (var data : chart.getData().get(0).getData()) {
             var date = data.getXValue().longValue();
             var duration = data.getYValue().longValue();
             
@@ -98,7 +103,7 @@ final class SleepLengthGraph extends AreaChart<Number, Number> {
             maxSeconds = Math.max(maxSeconds, duration);
         }
         
-        var xAxis = (NumberAxis) getXAxis();
+        var xAxis = (NumberAxis) chart.getXAxis();
         
         xAxis.setLowerBound(minDate);
         xAxis.setUpperBound(maxDate);
@@ -106,7 +111,7 @@ final class SleepLengthGraph extends AreaChart<Number, Number> {
         Log.notef("x axis: %s to %s", Tools.formatDate(Instant.ofEpochSecond(minDate)),
                                       Tools.formatDate(Instant.ofEpochSecond(maxDate)));
         
-        var yAxis = (NumberAxis) getYAxis();
+        var yAxis = (NumberAxis) chart.getYAxis();
         
         var yLowerBound = 0.0; // Math.floor(minSeconds / (double) SECS_IN_HR);
         var yUpperBound = Math.ceil(maxSeconds / (double) SECS_IN_HR);
@@ -116,25 +121,25 @@ final class SleepLengthGraph extends AreaChart<Number, Number> {
         Log.notef("y axis: %f hours to %f hours", yLowerBound, yUpperBound);
     }
     
-    private enum DataFunction implements Function<SleepPeriod, Data<Number, Number>> {
+    private enum DataFunction implements Function<SleepPeriod, AreaChart.Data<Number, Number>> {
         INSTANCE;
         
         @Override
-        public Data<Number, Number> apply(SleepPeriod period) {
+        public AreaChart.Data<Number, Number> apply(SleepPeriod period) {
             var start = period.getStart();
             var end = period.getEnd();
             
             var date = Tools.toStartOfDay(start).getEpochSecond();
             var duration = Duration.between(start, end).toSeconds();
             
-            return new Data<>(date, duration);
+            return new AreaChart.Data<>(date, duration);
         }
     }
     
     private void update() {
         Log.enter();
         var periods = db.getAllSleepPeriods();
-        var series = new Series<Number, Number>();
+        var series = new AreaChart.Series<Number, Number>();
         series.setName("Sleep Duration");
         
         var minDate = Long.MAX_VALUE;
@@ -153,12 +158,12 @@ final class SleepLengthGraph extends AreaChart<Number, Number> {
             minSeconds = Math.min(minSeconds, duration);
             maxSeconds = Math.max(maxSeconds, duration);
             
-            series.getData().add(new Data<>(date, duration));
+            series.getData().add(new AreaChart.Data<>(date, duration));
         }
         
-        setData(Tools.observableArrayList(series));
+        chart.setData(Tools.observableArrayList(series));
         
-        var xAxis = (NumberAxis) getXAxis();
+        var xAxis = (NumberAxis) chart.getXAxis();
         
         xAxis.setLowerBound(minDate);
         xAxis.setUpperBound(maxDate);
@@ -166,7 +171,7 @@ final class SleepLengthGraph extends AreaChart<Number, Number> {
         Log.notef("x axis: %s to %s", Tools.formatDate(Instant.ofEpochSecond(minDate)),
                                       Tools.formatDate(Instant.ofEpochSecond(maxDate)));
         
-        var yAxis = (NumberAxis) getYAxis();
+        var yAxis = (NumberAxis) chart.getYAxis();
         
         var yLowerBound = 0.0; // Math.floor(minSeconds / (double) SECS_IN_HR);
         var yUpperBound = Math.ceil(maxSeconds / (double) SECS_IN_HR);
