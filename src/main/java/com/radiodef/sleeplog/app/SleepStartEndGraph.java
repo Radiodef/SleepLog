475c6a3cac14c7d546397d4496ef06f03c45c97f
David <acynicalpie@gmail.com>
2019-01-03 16:26:15 -0500

    add chart

--- src/main/java/com/radiodef/sleeplog/app/SleepStartEndGraph.java
+++ src/main/java/com/radiodef/sleeplog/app/SleepStartEndGraph.java
@@ -3,13 +3,20 @@ package com.radiodef.sleeplog.app;
 import com.radiodef.sleeplog.db.*;
 
 import javafx.scene.layout.*;
+import javafx.scene.chart.*;
 
 import java.util.*;
 
 final class SleepStartEndGraph extends BorderPane {
     private final Database db;
     
+    private final LineChart<Number, Number> chart;
+    
     SleepStartEndGraph(Database db) {
         this.db = Objects.requireNonNull(db, "db");
+        
+        this.chart = new LineChart<>(GraphsPane.createDayAxis(), GraphsPane.createTimeAxis());
+        
+        setCenter(chart);
     }
 }
