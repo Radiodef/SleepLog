2980131874d9c6d6550bdc6a92a69059d5a61e87
David <acynicalpie@gmail.com>
2019-01-03 18:34:43 -0500

    add columns to pane

--- src/main/java/com/radiodef/sleeplog/util/PseudoTable.java
+++ src/main/java/com/radiodef/sleeplog/util/PseudoTable.java
@@ -10,7 +10,8 @@ import javafx.beans.property.*;
 public class PseudoTable<R> extends BorderPane {
     private static final String PSEUDO_TABLE = "pseudo-table";
     private static final String PANE_CLASS = PSEUDO_TABLE + "-pane";
-    private static final String COLS_BOX_CLASS = PSEUDO_TABLE + "-columns-box";
+    private static final String COLS_HBOX_CLASS = PSEUDO_TABLE + "-columns-hbox";
+    private static final String COL_VBOX_CLASS = PSEUDO_TABLE + "-column-vbox";
     
     private final ObservableList<Column<R>> columns = FXCollections.observableArrayList();
     
@@ -18,7 +19,7 @@ public class PseudoTable<R> extends BorderPane {
     
     public PseudoTable() {
         getStyleClass().add(PANE_CLASS);
-        columnsBox.getStyleClass().add(COLS_BOX_CLASS);
+        columnsBox.getStyleClass().add(COLS_HBOX_CLASS);
         
         columns.addListener(Tools.listChangeListener(c -> layoutComponents()));
         
@@ -37,8 +38,21 @@ public class PseudoTable<R> extends BorderPane {
     private void layoutComponents() {
         Log.enter();
         columnsBox.getChildren().clear();
+        
+        for (var col : columns) {
+            var box = col.nodeProperty().get();
+            
+            if (box == null) {
+                box = new VBox();
+                box.getStyleClass().add(COL_VBOX_CLASS);
+                col.nodeProperty().set(box);
+            }
+            
+            columnsBox.getChildren().add(box);
+        }
     }
     
+    @SuppressWarnings("WeakerAccess")
     public static class Column<R> {
         private final ObjectProperty<String> name;
         private final ObjectProperty<VBox> node;
