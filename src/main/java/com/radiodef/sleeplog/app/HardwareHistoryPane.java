b7be1cc5e5267be14141c6cf57d15644e72a1785
David <acynicalpie@gmail.com>
2019-01-18 15:57:38 -0500

    add pmset command execution

--- src/main/java/com/radiodef/sleeplog/app/HardwareHistoryPane.java
+++ src/main/java/com/radiodef/sleeplog/app/HardwareHistoryPane.java
@@ -2,7 +2,38 @@ package com.radiodef.sleeplog.app;
 
 import javafx.scene.layout.*;
 
+import java.util.*;
+import java.io.*;
+
 final class HardwareHistoryPane extends BorderPane {
     HardwareHistoryPane() {
+    }
+    
+    // https://apple.stackexchange.com/questions/52064/how-to-find-out-the-start-time-of-last-sleep
+    private static final String PMSET_ALL_COMMAND = "pmset -g log";
+    
+    static List<String> getHardwareHistory() {
+        var lines = new ArrayList<String>();
+        
+        try {
+            var proc = Runtime.getRuntime().exec(PMSET_ALL_COMMAND);
+            
+            try (var in = new BufferedReader(new InputStreamReader(proc.getInputStream()))) {
+                in.lines().forEach(lines::add);
+            }
+            try (var err = new BufferedReader(new InputStreamReader(proc.getErrorStream()))) {
+                for (String ln : (Iterable<String>) err.lines()::iterator) {
+                    Log.note(ln);
+                }
+            }
+            
+            var result = proc.waitFor();
+            Log.notef("process returned %d", result);
+            
+        } catch (IOException | InterruptedException x) {
+            Log.caught(x);
+        }
+        
+        return lines;
     }
 }
